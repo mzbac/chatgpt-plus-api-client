949d388792f42cb24f380d0220413e1cf0daa1df
Anchen <li.anchen.au@gmail.com>
2023-02-14 00:45:57 +1100

    update prompt

--- codeguard/prompt.ts
+++ codeguard/prompt.ts
@@ -10,7 +10,7 @@ export function promptForText(
 }
 
 export function promptForJson(sourceCodeWithLineNumber: string): string {
-  return `Act as a code guard that has deep knowledge of software development, you will review the pull request files change below for a project is written in Typescript. Please provide suggestions for making the code more readable,maintainable and secure in the format of a json object, property key of the json object uses the line number as key value and value of the property is the suggestion and reason without any code block.
+  return `Act as a code guard that has deep knowledge of software development, you will review the pull request files change below for a project is written in Typescript. Please provide suggestions for making the code more readable,maintainable and secure in the format of a json object, property key of the json object uses the line number as key value and value of the property is the suggestion and reason without any code block. please only reply the json object, not no additional text.
     \`\`\`ts
     ${sourceCodeWithLineNumber}
     \`\`\``;
