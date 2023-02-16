04416e4a50656ad952ed8ade58b9a2ec68aa892a
Anchen <li.anchen.au@gmail.com>
2023-02-16 21:21:55 +1100

    add check for extract json string from response text

--- codeguard/chatgptClient.ts
+++ codeguard/chatgptClient.ts
@@ -1,5 +1,6 @@
 import { sendPostRequest } from "../src";
 import { promptForJson } from "./prompt";
+import { extractJsonString } from "./utils";
 
 export async function getSuggestions(
   textWithLineNumber: string,
@@ -11,9 +12,12 @@ export async function getSuggestions(
       linesToReview.map(({ start, end }) => `line ${start}-${end}`).join(",")
     ),
   });
+
   let suggestions;
   try {
-    suggestions = JSON.parse(response.message.content.parts[0]);
+    suggestions = JSON.parse(
+      extractJsonString(response.message.content.parts[0])
+    );
   } catch (err) {
     throw new Error(
       `ChatGPT response is not a valid json:\n ${response.message.content.parts[0]}`
