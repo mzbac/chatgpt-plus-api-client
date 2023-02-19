f7bc00c7c5358f054ca0e7ac3d7a2619da073412
Anchen <li.anchen.au@gmail.com>
2023-02-20 01:14:15 +1100

    add extract json from string

--- codeguard/chatgptClient.ts
+++ codeguard/chatgptClient.ts
@@ -10,12 +10,20 @@ export async function getSuggestions(
       textWithLineNumber,
       linesToReview.map(({ start, end }) => `line ${start}-${end}`).join(",")
     ),
-    model:'Legacy'
+    model: "Legacy",
   });
+
+  const str = response.message.content.parts[0];
+  const startIndex = str.indexOf("{");
+  const endIndex = str.lastIndexOf("}");
+  let json;
+  if (startIndex !== -1 && endIndex !== -1 && endIndex > startIndex) {
+    json = str.substring(startIndex, endIndex + 1);
+  }
 
   let suggestions;
   try {
-    suggestions = JSON.parse(response.message.content.parts[0]);
+    suggestions = JSON.parse(json);
   } catch (err) {
     throw new Error(
       `ChatGPT response is not a valid json:\n ${response.message.content.parts[0]}`
